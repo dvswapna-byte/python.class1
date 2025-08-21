680625d4232c60e59c4afa08432a36ee87d19a8d
dvswapna <dv.swapna@gmail.com>
2025-08-21 13:24:04 +0530

    21 Aug basic python codes

---
+++ 01_python_basics/demo.py
@@ -0,0 +1,27 @@
+a=10
+b=20
+a+b
+
+
+
+
+a=20
+b=30
+print(a+b)
+
+
+print("Hello World")
+
+print(9)
+print(10)
+print(2.5)
+
+greetings = "Hello, Python"
+print(greetings)
+print("This is a demo script for Python basics.")
+
+
+
+#import demo
+import keyword
+
