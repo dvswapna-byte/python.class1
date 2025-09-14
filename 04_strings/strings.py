2961e755b886ad5d8e48a575a10428ff999e6196
dvswapna <dv.swapna@gmail.com>
2025-09-14 10:39:10 +0530

    stringpractice

---
+++ 04_strings/strings.py
@@ -0,0 +1,14 @@
+# strings
+# string is a sequence of characters
+a = "hello"
+b = 'world'
+c = '''hello world'''
+d = """hello world"""
+print(a)
+print(b)
+print(c)
+print(d)
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
