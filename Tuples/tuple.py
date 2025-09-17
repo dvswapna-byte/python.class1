8ff081d772bbc8d4717bbc87e156cba0f6a6122d
dvswapna <dv.swapna@gmail.com>
2025-09-17 19:20:28 +0530

    tuple practice

---
+++ Tuples/tuple.py
@@ -0,0 +1,19 @@
+# tuples in python
+# empty tuple
+empty_tuple = ()
+print(type(empty_tuple))
+print(empty_tuple)
+
+#store data in tuple
+num_tuple = (10,20,30,40,50)
+print(num_tuple)
+
+string_tuple = ["Python","Django","Scripting"]
+print(string_tuple)
+
+mixed_tuple = [10,20,"Python","Java",40,30,50]
+print(mixed_tuple)
+
+#indexing
+#slicing
+#memory
