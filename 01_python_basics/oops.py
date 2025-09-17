091088c591f7f835309a0320af7bbdd39278a352
dvswapna <dv.swapna@gmail.com>
2025-09-17 19:20:07 +0530

    17SEP

--- 01_python_basics/oops.py
+++ 01_python_basics/oops.py
@@ -3,3 +3,27 @@
 #extend
 #takes an iterable and adds each element to the list individually
 
+# reverse
+#reverses thebelements in the list
+# Sort: sort the elements of the list
+# reverse using slice technique
+# sort the elements in ascending or descending order, implace operation
+list_nums = [20, 10, 30, 50, 40]
+print(list_nums)
+print(list_nums.sort(reverse=True))
+print(list_nums)
+
+# sort with text
+list_courses = ["c", "Python","Java","Cloud"]
+print(list_courses)
+print(list_courses.sort())
+print(list_courses)
+
+#mixed data
+list_mix = [20, 10. "C","Python",30, 50, 40]
+print(list_mix)
+#print(list_mix.sort())
+print(list_mix)
+
+#copy: creates a shallow copt
+#soft copy
