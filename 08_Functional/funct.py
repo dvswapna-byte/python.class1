f721e81220d6f67c582d16492f3cd91ffc2c5667
dvswapna <dv.swapna@gmail.com>
2025-09-25 12:56:45 +0530

    function types

--- 08_Functional/funct.py
+++ 08_Functional/funct.py
@@ -72,6 +72,65 @@ emp_info("user2","user2@gmail.com")
 emp_info("Avaneesh","ava@gmail.com","Malaysia")
 
 
+# with keyword arguement
+
+def emp_info(emp_name="swapna",emp_email="swapna@gmail.com",emp_location="Hyderabad",emp_country="India"):
+    print(f"Hi {emp_name}, Your Email is {emp_email} and location is {emp_location} in {emp_country}")
+emp_info(emp_name="swapna",emp_email="swapna@gmail.com",emp_location="Hyderabad",emp_country="India")
+
+# Arbitary Positional (*arge)
+def add_two_nums(a,b):
+    print(a+b)
+
+def add_unknown(*numbers):
+    sum = 0
+    for i in numbers:
+        sum = sum + i
+    print(f"Sum is: {sum}")
+
+add_unknown()
+add_unknown(1)
+add_unknown(1,2,3)
+add_unknown(10,20,56)
+
+#Arbitary Keyword Arguement (**kwargs)
+def profile(**info):
+    print(info)
+
+profile()
+profile(name="swapna")
+profile(name="swapna",id=236,city="Hyderabad")
+
+# Transactions
+def cred_transactions(**transactions):
+    print(transactions)
+    total = 0
+    for i in transactions:
+        total = total + transactions[i]
+        print(f"You have done {len(transactions)} transactions and total valueof all transactions is {total}")
+
+cred_transactions(jan=1000,feb=15000,mar=500)
+
+# using arg and kwarg
+def cred_transactions_new(*transactions,**account):
+    print(transactions)
+    print(account)
+    total = 0
+    for i in transactions:
+        total = total + 1
+    print(f"Hi {account['name']} You have done {total} amount of transactios in {account['account_id']}")
+cred_transactions_new(1000,2000,3000,name="swapna",account_id=325892156)
+
+
+        
+
+
+
+
+    
+
+
+
 
 
 
