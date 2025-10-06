40cafbafdac0f8c75cbe66cb430c318972604cb6
dvswapna <dv.swapna@gmail.com>
2025-10-06 20:22:56 +0530

    module syntax

---
+++ 10_MODULES/my_module.py
@@ -0,0 +1,10 @@
+# user defined module
+name = "swapna"
+email = "swapna@gmail.com"
+
+def add(a,b):
+    return a+b
+def prod(a,b):
+    return a*b
+def profile():
+    return f"Hi {name}, your registered email is {email}"
